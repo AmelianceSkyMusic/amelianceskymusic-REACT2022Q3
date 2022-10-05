97ab1e14fe6d886a4758ebcf9f7934ec3b291078
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-10-05 22:41:58 +0300

    feat: add Card

---
+++ react-01-components/src/App/components/Card.tsx
@@ -0,0 +1,26 @@
+import './Card.css';
+import React, { Component } from 'react';
+import { ICard } from 'App/types/ICard';
+
+export class Card extends Component<ICard> {
+  render() {
+    const { balance, brand, color, name, popular, price, size, year, image } = this.props;
+
+    return (
+      <div className="card">
+        <div className="card__visual">
+          <img className="card__brand" src={`assets/svg/${brand}.svg`} alt={image || 'brand'} />
+          <img className="card__img" src={`assets/img/${image}.png`} alt={image || 'img'} />
+        </div>
+        <div className="card__info">
+          <h2>{name}</h2>
+          <p>COLOR: {color}</p>
+          <p>{popular}</p>
+          <p>PRICE: ${price}</p>
+          <p>SIZE: {size}</p>
+          <p>{year}</p>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ react-01-components/src/App/types/ICard.ts
@@ -0,0 +1,12 @@
+export interface ICard {
+  balance: string | null;
+  brand: string | null;
+  color: string | null;
+  id: string | null;
+  image: string | null;
+  name: string | null;
+  popular: string | null;
+  price: string | null;
+  size: string | null;
+  year: string | null;
+}

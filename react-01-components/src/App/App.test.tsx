6f9e8ef9820f894f4873d3b372a367d3af439e43
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-10-17 12:19:22 +0300

    feat: add tests

--- react-01-components/src/App/App.test.tsx
+++ react-01-components/src/App/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('app should render navigation', () => {
+    render(<App />, { wrapper: BrowserRouter });
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout.getAttribute('href')).toBe('/about');
+  });
+  it('show 404 if bad route', () => {
+    const badRoute = '/some/bad/route';
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/404/i)).toBeInTheDocument();
+  });
 });
---
+++ react-01-components/src/App/pages/Main.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Main } from './Main';
+import api from 'App/api';
+class LocalStorage {
+  store: { [key: string]: string };
+  constructor() {
+    this.store = {};
+  }
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+  setItem(key: string, value: string) {
+    this.store[key] = value.toString();
+  }
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+  clear() {
+    this.store = {};
+  }
+}
+describe('Local Storage', () => {
+  Object.defineProperty(window, 'localStorage', {
+    value: new LocalStorage(),
+  });
+  it('should set and get data', async () => {
+    const { rerender } = render(<Main />);
+    await screen.findAllByText(/nike/i);
+    userEvent.type(screen.getByPlaceholderText('Search'), 'Nike');
+    rerender(<Main />);
+    expect(screen.getByPlaceholderText('Search')).toHaveDisplayValue('Nike');
+    userEvent.clear(screen.getByPlaceholderText('Search'));
+    userEvent.type(screen.getByPlaceholderText('Search'), 'Reebok');
+    rerender(<Main />);
+    expect(screen.getByPlaceholderText('Search')).toHaveDisplayValue('Reebok');
+    userEvent.click(screen.getByRole('button'));
+  });
+});
+describe('Fetch', () => {
+  it('should return data', async () => {
+    const response = await api.google.getSheetData({
+      sheetId: '11IF6n311xG3ycdE_mOQaZizL7NFzeynvFu2ni1sghQ0',
+    });
+    expect(response).not.toBeNull();
+  });
+  it('should return error', async () => {
+    const response = await api.google.getSheetData({
+      sheetId: '00000000000000',
+    });
+    expect(response).toBeUndefined();
+  });
+});

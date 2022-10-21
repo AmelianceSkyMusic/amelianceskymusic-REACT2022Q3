6d190fc089e946621af718e13347fa6970d19bf8
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-10-21 14:26:40 +0300

    refactor: move tests to folders

--- react-02-forms/src/App/components/__test__/Loading.test.tsx
+++ react-02-forms/src/App/components/__test__/Loading.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Loading } from './Loading';
+import { Loading } from '../Loading';
 describe('Search component', () => {
   it('renders learn react link', () => {
     render(<Loading />);

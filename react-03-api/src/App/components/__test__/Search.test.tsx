ca36696619d2b17f1d2a523d3741e06b05987b7b
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-10-28 14:52:02 +0300

    refactor: update Search test

--- react-03-api/src/App/components/__test__/Search.test.tsx
+++ react-03-api/src/App/components/__test__/Search.test.tsx
@@ -2,36 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Search } from '../Search';
-import { Main } from '../../pages/Main';
-const onChange = jest.fn();
-const searchApply = jest.fn();
+
+const handleSearchApply = jest.fn();
+const handleSearchOnChange = jest.fn();
+
 describe('Search component', () => {
   it('search render', () => {
-    render(<Search value="" onChange={onChange} searchApply={searchApply} />);
+    render(<Search value="" onApply={handleSearchApply} onChange={handleSearchOnChange} />);
+    expect(screen.getByRole('searchbox')).toBeInTheDocument();
   });
+
   it('should onChange works', () => {
-    render(<Search value="" onChange={onChange} searchApply={searchApply} />);
-    userEvent.type(screen.getByRole('searchbox'), 'react');
-    expect(onChange).toHaveBeenCalledTimes(5);
+    render(<Search value="" onApply={handleSearchApply} onChange={handleSearchOnChange} />);
+    userEvent.type(screen.getByRole('searchbox'), 'Cars');
+    expect(handleSearchOnChange).toHaveBeenCalledTimes(4);
   });
+
   it('typing in searchbox work', () => {
-    render(<Search value="" onChange={onChange} searchApply={searchApply} />);
-    expect(screen.queryByDisplayValue(/nike/i)).toBeNull();
-    userEvent.type(screen.getByRole('searchbox'), 'Nike');
-    expect(screen.queryByDisplayValue(/nike/i)).toBeInTheDocument();
-  });
-  it('Search works', async () => {
-    render(<Main />);
-    screen.queryAllByText(/reebok/i);
-    screen.queryAllByText(/nike/i);
-    const itemsNike = await screen.findAllByText(/nike/i);
-    itemsNike.forEach((item) => {
-      expect(item).toBeInTheDocument();
-    });
-    screen.queryAllByText(/reebok/i);
-  });
-  it('button exist on page', () => {
-    render(<Search value="" onChange={onChange} searchApply={searchApply} />);
-    screen.getByRole('button', { name: /search/i });
+    render(<Search value="" onApply={handleSearchApply} onChange={handleSearchOnChange} />);
+    expect(screen.queryByDisplayValue(/react/i)).toBeNull();
+    userEvent.type(screen.getByRole('searchbox'), 'Cars');
+    expect(screen.queryByDisplayValue(/cars/i)).toBeInTheDocument();
   });
 });

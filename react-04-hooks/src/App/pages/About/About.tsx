47ed89ee08629dc1564d971ecf6c8f23e952e948
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-04 16:44:08 +0200

    refactor: update About page

---
+++ react-04-hooks/src/App/pages/About/About.tsx
@@ -0,0 +1,13 @@
+import './About.scss';
+import React from 'react';
+
+export function About() {
+  return (
+    <main className="about-page main">
+      <div className="container">
+        <h1 className="h1 title">About</h1>
+        <p className="h1 arrow">↓</p>
+      </div>
+    </main>
+  );
+}

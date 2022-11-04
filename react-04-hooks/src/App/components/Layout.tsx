07e37e8aa3c4a4c64dacdc08281f6458c35e93f9
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-04 16:44:34 +0200

    refactor: update Layout page

--- react-04-hooks/src/App/components/Layout.tsx
+++ react-04-hooks/src/App/components/Layout.tsx
@@ -1,20 +1,36 @@
+import './Layout.scss';
 import React, { Component } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import './Layout.css';
 
 export class Layout extends Component {
   render() {
     return (
       <>
         <header className="header">
-          <NavLink end to="/">
+          <NavLink className="p1" end to="/">
             Main
           </NavLink>
-          <NavLink to="/form">Form</NavLink>
-          <NavLink to="/about">About</NavLink>
+          <NavLink className="p1" to="/form">
+            Form
+          </NavLink>
+          <NavLink className="p1" to="/about">
+            About
+          </NavLink>
+          <NavLink className="p1" to="/404">
+            404
+          </NavLink>
         </header>
         <Outlet />
-        <footer className="footer">2022</footer>
+        <footer className="footer">
+          <a
+            href="https://amelianceskymusic.github.io/"
+            target="_blank"
+            className="link link__underlined "
+            rel="noreferrer"
+          >
+            AmelianceSkyMusic © 2022
+          </a>
+        </footer>
       </>
     );
   }

f183b1764db6c981e06816447e9e316cfe5e2daa
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-04 16:43:40 +0200

    refactor: update NonFound page

---
+++ react-04-hooks/src/App/pages/NotFound/NotFound.tsx
@@ -0,0 +1,15 @@
+import './NotFound.scss';
+import React from 'react';
+
+export function NotFound() {
+  return (
+    <main className="notfound-page main">
+      <div className="container">
+        <div className="title">
+          <h1 className="h1 title__40">40</h1>
+          <h1 className="h1 title__4">4</h1>
+        </div>
+      </div>
+    </main>
+  );
+}

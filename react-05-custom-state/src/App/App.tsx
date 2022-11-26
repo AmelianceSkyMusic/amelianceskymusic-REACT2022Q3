2ba025cbf02c79432e79fdc31bd4659d722b6e0e
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-26 17:55:20 +0200

    refactor: connect store

--- react-05-custom-state/src/App/App.tsx
+++ react-05-custom-state/src/App/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { About } from './pages/About';
@@ -7,26 +7,29 @@ import { Main } from './pages/Main';
 import { Form } from './pages/Form';
 import { NotFound } from './pages/NotFound';
 import { MainPageProvider } from './store/MainPageState';
-import { MainCardPage } from './pages/Main/MainCardPage';
 import { MainCardPageLayout } from './pages/Main/MainCardPageLayout';
+import { MainCardPage } from './pages/Main/MainCardPage';
+import { FormPageProvider } from './store/FormPageState/FormPageProvider';
 
 export function App() {
   return (
     <MainPageProvider>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Main />} />
-            <Route path="about" element={<About />} />
-            <Route path="form" element={<Form />} />
-            <Route path="404" element={<NotFound />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route path="/" element={<MainCardPageLayout />}>
-            <Route path="card/:id" element={<MainCardPage />} />
-          </Route>
-        </Routes>
-      </div>
+      <FormPageProvider>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Main />} />
+              <Route path="about" element={<About />} />
+              <Route path="form" element={<Form />} />
+              <Route path="404" element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+            <Route path="/" element={<MainCardPageLayout />}>
+              <Route path="card/:id" element={<MainCardPage />} />
+            </Route>
+          </Routes>
+        </div>
+      </FormPageProvider>
     </MainPageProvider>
   );
 }

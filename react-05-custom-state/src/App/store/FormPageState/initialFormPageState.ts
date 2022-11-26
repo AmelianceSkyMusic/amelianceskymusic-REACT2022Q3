dc4046a998535f02908d2bbadd1e864475866954
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-26 17:54:10 +0200

    feat: add Form page state

---
+++ react-05-custom-state/src/App/store/FormPageState/initialFormPageState.ts
@@ -0,0 +1,12 @@
+export const initialFormPageState = {
+  form: {
+    firstName: '',
+    birthday: '',
+    framework: '',
+    good: false,
+    showSex: false,
+    sex: undefined,
+    avatar: undefined,
+  },
+  cards: [],
+};

d8439fe3993547de2fdd3a866547e899b560ff91
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-27 13:01:43 +0200

    refactor: add form types

---
+++ react-06-redux/src/App/types/IFormCard.ts
@@ -0,0 +1,9 @@
+export interface IFormCard {
+  firstName: string;
+  birthday: string;
+  framework: string;
+  good: boolean;
+  sex: string;
+  avatar: string;
+  id: number;
+}
---
+++ react-06-redux/src/App/types/IFormInputs.ts
@@ -0,0 +1,9 @@
+export interface IFormInputs {
+  firstName: string;
+  birthday: string;
+  framework: string;
+  good: boolean;
+  showSex: boolean;
+  sex: string | undefined;
+  avatar: FileList | undefined;
+}
